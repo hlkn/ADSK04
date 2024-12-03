d1f37f1859cbbd371b77cbfebf432955fc9fc805
henrynguyen <henrylknguyen@gmail.com>
2024-12-03 15:14:50 +1100

    ADSK040101 Implement Home, Tours and Shop Fragments logic and resources.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,22 +5,80 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : FragmentActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState != null) return
+
+        supportFragmentManager.commit {
+            add<HomeFragment>(R.id.container, null)
+        }
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> goToHome()
+
+                R.id.action_tours -> goToTours()
+
+                R.id.action_shop -> goToShop()
+
+                else -> false
+            }
+        }
+
+        val viewModel = ViewModelProvider(this)[SharedViewModel::class.java]
+        viewModel.quantity.observe(this) {
+            updateBadge(it)
         }
     }
+
+    private fun updateBadge(count: Int) {
+        val badge = binding.bottomNavigation.getOrCreateBadge(R.id.action_shop)
+        if (count > 0) {
+            badge.number = count
+            badge.isVisible = true
+        } else {
+            badge.clearNumber()
+            badge.isVisible = false
+        }
+    }
+
+    private fun goToShop(): Boolean {
+        supportFragmentManager.commit {
+            replace<ShopFragment>(R.id.container, null, null)
+        }
+
+        return true
+    }
+
+    private fun goToTours(): Boolean {
+        supportFragmentManager.commit {
+            replace<ToursFragment>(R.id.container, null, null)
+        }
+
+        return true
+    }
+
+    private fun goToHome(): Boolean {
+        supportFragmentManager.commit {
+            replace<HomeFragment>(R.id.container, null, null)
+        }
+
+        return true
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/ToursFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentToursBinding
+
+
+class ToursFragment : Fragment() {
+    private var _binding: FragmentToursBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentToursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
