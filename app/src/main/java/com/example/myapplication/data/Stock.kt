48e3f3c526021f33aad84285d78833575b06081c
henrynguyen <henrylknguyen@gmail.com>
2024-12-05 20:02:07 +1100

    ADSK040107 Using moshi library to process Json file

--- app/src/main/java/com/example/myapplication/data/Stock.kt
+++ app/src/main/java/com/example/myapplication/data/Stock.kt
@@ -1,4 +1,10 @@
 package com.example.myapplication.data
 
-class Stock {
-}
+import com.squareup.moshi.Json
+
+data class Stock(
+    @Json(name = "firm_id") val firmId: Int,
+    val open: Double,
+    val close: Double,
+    val change: Double
+)
