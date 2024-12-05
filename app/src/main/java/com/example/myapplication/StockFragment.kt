7ee15746dabe24442940b33e50a92994944aa006
henrynguyen <henrylknguyen@gmail.com>
2024-12-05 15:40:18 +1100

    ADSK040106 Implement the Stock Fragment

---
+++ app/src/main/java/com/example/myapplication/StockFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentStockBinding
+
+
+class StockFragment : Fragment() {
+
+    private var _binding: FragmentStockBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentStockBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+
+}
