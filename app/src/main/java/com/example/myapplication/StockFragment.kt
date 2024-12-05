eda697cd9e8005d9731dc634f1b96548e6785c94
henrynguyen <henrylknguyen@gmail.com>
2024-12-05 20:02:49 +1100

    ADSK040107 Implement on click listener to display data of various format

--- app/src/main/java/com/example/myapplication/StockFragment.kt
+++ app/src/main/java/com/example/myapplication/StockFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.myapplication.data.Stock
 import com.example.myapplication.databinding.FragmentStockBinding
 
 
@@ -12,6 +14,8 @@ class StockFragment : Fragment() {
 
     private var _binding: FragmentStockBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +25,42 @@ class StockFragment : Fragment() {
         _binding = FragmentStockBinding.inflate(inflater, container, false)
         val view = binding.root
 
+
+        viewModel.stockInfo.observe(viewLifecycleOwner, {
+            updateStockInfo(it)
+        })
+
+        binding.firm1Btn.setOnClickListener {
+            viewModel.getStockData(firmId = 1)
+        }
+        binding.firm2Btn.setOnClickListener {
+            viewModel.getStockData(firmId = 2)
+        }
+        binding.firm3Btn.setOnClickListener {
+            viewModel.getStockData(firmId = 3)
+        }
         return view
     }
 
+    private fun updateStockInfo(stock: Stock) {
+        binding.firmLabelText.text = when (stock.firmId) {
+            1 -> getString(R.string.firm_1_btn_label)
+            2 -> getString(R.string.firm_2_btn_label)
+            3 -> getString(R.string.firm_3_btn_label)
+            else -> ""
+        }
+        binding.stockInfoText.text = getString(
+            R.string.stock_info,
+            stock.open,
+            stock.close,
+            stock.change
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
+
